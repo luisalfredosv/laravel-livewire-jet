3d7894f78c7c5150b52d051561ded52105961df9
luisalfredosv <luissarabia80@gmail.com>
2021-10-08 15:42:09 -0400

    [update] Modelo Producto

---
+++ app/Models/Producto.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Producto extends Model
+{
+    use HasFactory;
+    protected $fillable = ['descripcion', 'cantidad'];
+
+    public function run()
+    {
+        User::factory()
+            ->count(50)
+            ->hasPosts(1)
+            ->create();
+    }
+}
